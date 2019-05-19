03a3683d6a9b2037ae5938593326091347a32ead
tohue <tonegrep@gmail.com>
2019-05-19 15:07:33 +0300

    init

---
+++ bigbrother_common.h
@@ -0,0 +1,47 @@
+#define HOST_TRANSMIT "http://192.168.1.10:8000/hardware/transmit"
+#define HOST_STATUS_REPORT "http://192.168.1.10:8000/hardware/status"
+
+#define STATUS_READY   0
+#define STATUS_BUSY    1
+#define STATUS_OFFLINE 2
+
+
+#define JOB_CLEAR                100
+#define STATUS_CHANGED           101
+#define ERROR_PIN_WRITE          200
+#define ERROR_PIN_READ           201
+#define ERROR_BAD_REQUEST        202
+#define ERROR_SERVER_UNAVAILABLE 203
+#define ERROR_CLIENT_UNAVAILABLE 204
+#define ERROR_BUSY               205
+
+typedef struct {
+    String UUID;
+    int    status_code;
+    int    response_code;
+    int    data;
+} json_info_t;
+
+#define HTML_INFO(str, name, x) str = "<!DOCTYPE HTML>\r\n<html>\r\n"; \
+  str += "<h3>current "; \
+  str += name; \
+  str += " is "; \
+  str += x; \
+  str += "</h3>"; \
+  str += "</html>\n";
+
+
+#define HTML_INFO_CHANGABLE(str, name, x) str = "<!DOCTYPE HTML>\r\n<html>\r\n"; \
+  str += "<h3>current "; \
+  str += name; \
+  str += " is "; \
+  str += x; \
+  str += "</h3>"; \
+  str += "<form action=\"SET\" method=\"post\">"; \
+  str += "<h5> Change this: </h5>"; \
+  str += "<input name=\"brightness\">"; \
+  str += "<button type=\"submit\" value=\"SET\"> Set </button>"; \
+  str += "</html>\n";
+  
+  
+
---
+++ bigbrother_tsens.ino
@@ -0,0 +1,96 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <ESP8266mDNS.h>
+#include <bigbrother_common.h>
+#include <Timer.h>
+
+const char* CONTROLLER_UUID = "PefwR97iuzyUPqZZlQ3W";
+int humidityPin = D5;
+int CONTROLLER_PORT = 302;
+int CURRENT_HUMIDITY;
+const char* SSID = "aaaaaaaaaaaaaaa";
+const char* PASSWORD = "89153510369";
+const char* GLOBAL_IP;
+const char* LOCAL_IP;
+int STATUS_CODE;
+MDNSResponder mdns;
+ESP8266WebServer server(CONTROLLER_PORT);
+
+Timer timer_send_data;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  
+  // Connect to WiFi network
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  // Setup mdns
+  if (mdns.begin("esp8266WebForm", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+  // Bound functions to requests
+  server.on("/", page);
+  server.on("/SET", set_brightness);
+  server.on("/GET", get_brightness);
+
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
+  timer_send_data.every(10000, send_data, 0);
+}
+
+void loop() {
+  server.handleClient();
+  timer_send_data.update();
+}
+
+void page() {
+   String s = "<!DOCTYPE HTML>\r\n<html>\r\n";
+   s += "<h3>current humidity is ";
+   s += CURRENT_HUMIDITY;
+   s += "</h3>";
+   s += "</html>\n";
+  respond_ok("text/html", s);
+}
+
+void get_humidity() {
+  String response = String(CURRENT_HUMIDITY);
+  respond_ok("text/plain", response);
+}
+
+void send_data(void* context) {
+  HTTPClient http;
+  http.begin(HOST_TRANSMIT);
+  http.addHeader("Content-Type", "text/json");
+  int httpCode = http.POST(create_response(JOB_CLEAR, CURRENT_HUMIDITY));
+  String payload = http.getString();
+  http.end();
+}
+
+void respond_ok(String response_type, String response){
+  server.sendHeader("Connection", "close");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, response_type, response);
+}
+
+String create_response(int response_code, int data) {
+  const int capacity = JSON_OBJECT_SIZE(4);
+  StaticJsonDocument<capacity> doc;
+  doc["UUID"] = CONTROLLER_UUID;
+  doc["status"] = STATUS_CODE;
+  doc["response_code"] = response_code;
+  doc["data"] = data;
+  String output;
+  serializeJson(doc, output);
+  return output;
+}
